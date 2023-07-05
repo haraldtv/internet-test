3c4a26d7ef1894ddd748208cf00f22224320aa66
Harald Tverdal <harald@tverdal.com>
2023-07-05 16:58:20 +0200

    Fixed formating and logging :D

--- internet_test.py
+++ internet_test.py
@@ -4,8 +4,12 @@ import subprocess
 import time
 import re
 
-with open("log_internet.txt",'w') as file:
+with open("log_verbose.txt",'w') as file:
     pass
+
+with open("log_rawdata.txt",'w') as file:
+    pass
+
 
 #iPerf server address
 IP_ADDR = '62.210.18.40'
@@ -35,23 +39,31 @@ while (iterator*TEST_INTERVAL != MAX_RUNTIME):
 
     speed = re.search('MBytes(.*)Mbits', str(output))
     speed = str(speed.group(1)).strip()
+    size = re.search('sec(.*)MBytes', str(output))
+    size = str(size.group(1)).strip()
+    length = re.search('0.00-(.* )sec', str(output))
+    length = str(length.group(1)).strip()
     #print(speed.group(1))
 
-    print("Formated output:" + speed) 
-    with open("internet_log.txt", "a") as myfile:
+    print("Formated output: " + length + " sec - " + size + " MBytes - " + speed + " Mbit/s") 
+    with open("log_verbose.txt", "a") as myfile:
         myfile.write(str(now))
 
         myfile.write('\n')
 
-        myfile.write(speed)
+        myfile.write(length + " sec - " + size + " MBytes - " + speed + " Mbit/s")
 
-        myfile.write('\n')
+        myfile.write("\n")
 
         myfile.write("----------")
 
         myfile.write('\n')
         myfile.write('\n')
 
+
+    with open("log_rawdata.txt", "a") as fp:
+        fp.write(speed)
+        fp.write("\n")
 
 
     time.sleep(60*TEST_INTERVAL)
